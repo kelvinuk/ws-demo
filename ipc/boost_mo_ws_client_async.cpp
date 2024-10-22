4d15475ca497e898fc55fb44a019f28a1678f3ae
Kelvin Chow <kelvinchowkafai@gmail.com>
2024-10-22 09:41:30 +0800

    Update boost_mo_ws_client_async.cpp

--- ipc/boost_mo_ws_client_async.cpp
+++ ipc/boost_mo_ws_client_async.cpp
@@ -1,18 +1,3 @@
-//
-// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// Official repository: https://github.com/boostorg/beast
-//
-
-//------------------------------------------------------------------------------
-//
-// Example: WebSocket client, asynchronous
-//
-//------------------------------------------------------------------------------
-
 #include "./boost_mo_ws_client_async.hpp"
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/core.hpp>
